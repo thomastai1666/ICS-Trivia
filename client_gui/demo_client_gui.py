8a8b344b1a776e1d19a96fa79a29a8fc2538aa21
Thomas Tai <tt1666@nyu.edu>
2018-04-27 18:10:10 +0800

    Updated gui

--- client_gui/demo_client_gui.py
+++ client_gui/demo_client_gui.py
@@ -5,32 +5,36 @@ from appJar import gui
 def press(button):
     if button == "Exit Game":
         app.stop()
-    elif button == "One" or button == "Two" or button == "Three":
+    elif button == "One" or button == "Two" or button == "Three" or button == "Four":
         print("Answer", button)
     elif button == "Send":
-        print("Chat Event: Send")
-    else:
         chat = app.getEntry("Chat")
         print("Chat Event:", chat)
+        app.clearEntry("Chat")
+    else:
+        print("Else")
 
 # create a GUI variable called app
 app = gui("Trivia", "400x600")
 app.setBg("white")
-app.setFont(18)
+app.setFont(20)
 
 # add & configure widgets - widgets get a name, to help referencing them later
 app.addLabel("title", "Welcome to Trivia")
 app.setLabelBg("title", "purple")
 app.setLabelFg("title", "white")
 
-app.addLabelEntry("Chat")
-
 # link the buttons to the function called press
 
+app.addLabel("q1", "Question #1")
 app.addButton("One", press)
+app.addLabel("q2", "Question #2")
 app.addButton("Two", press)
+app.addLabel("q3", "Question #3")
 app.addButton("Three", press)
+app.addLabel("q4", "Question #4")
 app.addButton("Four", press)
+app.addLabelEntry("Chat")
 app.addButtons(["Send", "Exit Game"], press)
 
 app.setFocus("Chat")
